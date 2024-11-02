161a1638a3bcf5c94b6261aadecfdccec7af093c
sixlive <tj@tjmiller.co>
2024-11-02 10:10:17 -0400

    Groq support

--- src/Providers/Groq/Client.php
+++ src/Providers/Groq/Client.php
@@ -38,6 +38,7 @@ class Client
         int|float|null $temperature,
         int|float|null $topP,
         ?array $tools,
+        string|array|null $toolChoice,
     ): Response {
         return $this->client->post(
             'chat/completions',
@@ -49,6 +50,7 @@ class Client
                 'temperature' => $temperature,
                 'top_p' => $topP,
                 'tools' => $tools,
+                'tool_choice' => $toolChoice,
             ]))
         );
     }
