d3ba189743a0dec8580aedd386397013f239f5c2
Chris Bridges <chris.bridges@tacit.legal>
2025-02-13 11:32:54 +0000

    fix docblock

--- src/Embeddings/Request.php
+++ src/Embeddings/Request.php
@@ -16,6 +16,7 @@ readonly class Request implements PrismRequest
     /**
      * @param  array<string, mixed>  $clientOptions
      * @param  array{0: array<int, int>|int, 1?: Closure|int, 2?: ?callable, 3?: bool}  $clientRetry
+     * @param  array<string, mixed>  $providerMeta
      */
     public function __construct(
         public string $model,
