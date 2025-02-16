9c2c2267e1e0440256a374639e4f114cbe508bb7
Pushpak Chhajed <pushpak1300@gmail.com>
2025-02-16 21:27:35 +0530

    feat(refactor): mistral provider

--- src/Providers/Mistral/Handlers/Text.php
+++ src/Providers/Mistral/Handlers/Text.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace EchoLabs\Prism\Providers\Mistral\Handlers;
 
+use EchoLabs\Prism\Concerns\CallsTools;
+use EchoLabs\Prism\Enums\FinishReason;
 use EchoLabs\Prism\Exceptions\PrismException;
 use EchoLabs\Prism\Providers\Mistral\Maps\FinishReasonMap;
 use EchoLabs\Prism\Providers\Mistral\Maps\MessageMap;
 use EchoLabs\Prism\Providers\Mistral\Maps\ToolChoiceMap;
 use EchoLabs\Prism\Providers\Mistral\Maps\ToolMap;
 use EchoLabs\Prism\Text\Request;
-use EchoLabs\Prism\ValueObjects\ProviderResponse;
+use EchoLabs\Prism\Text\Response as TextResponse;
+use EchoLabs\Prism\Text\ResponseBuilder;
+use EchoLabs\Prism\Text\Step;
+use EchoLabs\Prism\ValueObjects\Messages\AssistantMessage;
+use EchoLabs\Prism\ValueObjects\Messages\ToolResultMessage;
 use EchoLabs\Prism\ValueObjects\ResponseMeta;
 use EchoLabs\Prism\ValueObjects\ToolCall;
 use EchoLabs\Prism\ValueObjects\Usage;
@@ -20,41 +26,75 @@ use Throwable;
 
 class Text
 {
-    public function __construct(protected PendingRequest $client) {}
+    use CallsTools;
 
-    public function handle(Request $request): ProviderResponse
+    protected ResponseBuilder $responseBuilder;
+
+    public function __construct(protected PendingRequest $client)
     {
-        try {
-            $response = $this->sendRequest($request);
-        } catch (Throwable $e) {
-            throw PrismException::providerRequestError($request->model, $e);
-        }
+        $this->responseBuilder = new ResponseBuilder;
+    }
 
-        $data = $response->json();
+    public function handle(Request $request): TextResponse
+    {
+        $currentRequest = $request;
+        $stepCount = 0;
 
-        if (! $data || data_get($data, 'object') === 'error') {
-            throw PrismException::providerResponseError(vsprintf(
-                'Mistral Error:  [%s] %s',
-                [
-                    data_get($data, 'type', 'unknown'),
-                    data_get($data, 'message', 'unknown'),
-                ]
+        do {
+            $stepCount++;
+            try {
+                $response = $this->sendRequest($currentRequest);
+            } catch (Throwable $e) {
+                throw PrismException::providerRequestError($request->model, $e);
+            }
+
+            $data = $response->json();
+
+            if (! $data || data_get($data, 'object') === 'error') {
+                throw PrismException::providerResponseError(vsprintf(
+                    'Mistral Error:  [%s] %s',
+                    [
+                        data_get($data, 'type', 'unknown'),
+                        data_get($data, 'message', 'unknown'),
+                    ]
+                ));
+            }
+
+            $text = data_get($data, 'choices.0.message.content') ?? '';
+            $toolCalls = $this->mapToolCalls(data_get($data, 'choices.0.message.tool_calls', []) ?? []);
+            $finishReason = FinishReasonMap::map(data_get($data, 'choices.0.finish_reason', ''));
+            $toolResults = [];
+
+            $responseMessage = new AssistantMessage($text, $toolCalls, []);
+            $this->responseBuilder->addResponseMessage($responseMessage);
+            $currentRequest = $currentRequest->addMessage($responseMessage);
+
+            if ($finishReason === FinishReason::ToolCalls) {
+                $toolResults = $this->callTools($currentRequest->tools, $toolCalls);
+                $toolResultMessage = new ToolResultMessage($toolResults);
+                $currentRequest = $currentRequest->addMessage($toolResultMessage);
+            }
+
+            $this->responseBuilder->addStep(new Step(
+                text: $text,
+                finishReason: $finishReason,
+                toolCalls: $toolCalls,
+                toolResults: $toolResults,
+                usage: new Usage(
+                    data_get($data, 'usage.prompt_tokens'),
+                    data_get($data, 'usage.completion_tokens'),
+                ),
+                responseMeta: new ResponseMeta(
+                    id: data_get($data, 'id'),
+                    model: data_get($data, 'model'),
+                ),
+                messages: $currentRequest->messages,
+                additionalContent: []
             ));
-        }
 
-        return new ProviderResponse(
-            text: data_get($data, 'choices.0.message.content') ?? '',
-            toolCalls: $this->mapToolCalls(data_get($data, 'choices.0.message.tool_calls', []) ?? []),
-            usage: new Usage(
-                data_get($data, 'usage.prompt_tokens'),
-                data_get($data, 'usage.completion_tokens'),
-            ),
-            finishReason: FinishReasonMap::map(data_get($data, 'choices.0.finish_reason', '')),
-            responseMeta: new ResponseMeta(
-                id: data_get($data, 'id'),
-                model: data_get($data, 'model'),
-            )
-        );
+        } while ($stepCount < $request->maxSteps && $finishReason === FinishReason::ToolCalls);
+
+        return $this->responseBuilder->toResponse();
     }
 
     public function sendRequest(Request $request): Response
