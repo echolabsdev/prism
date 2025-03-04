63d281c46ad0fb6a556f7f60679d7bc1228a145d
Chris Bridges <chris.bridges@tacit.legal>
2025-03-04 20:37:53 +0000

    bug(gemini): fix schema map and add tests

--- src/Providers/Gemini/Maps/SchemaMap.php
+++ src/Providers/Gemini/Maps/SchemaMap.php
@@ -20,9 +20,9 @@ class SchemaMap
     public function toArray(): array
     {
         return array_merge([
-            'type' => $this->mapType(),
             ...array_filter([
                 ...$this->schema->toArray(),
+                'type' => $this->mapType(),
                 'additionalProperties' => null,
             ]),
         ], array_filter([
@@ -35,22 +35,20 @@ class SchemaMap
                     $property->name() => (new self($property))->toArray(),
                 ], []) :
                 null,
+            'nullable' => property_exists($this->schema, 'nullable')
+                ? $this->schema->nullable
+                : null,
         ]));
     }
 
     protected function mapType(): string
     {
-        switch ($this->schema::class) {
-            case ArraySchema::class:
-                return 'array';
-            case BooleanSchema::class:
-                return 'boolean';
-            case NumberSchema::class:
-                return 'number';
-            case ObjectSchema::class:
-                return 'object';
-            default:
-                return 'string';
-        }
+        return match ($this->schema::class) {
+            ArraySchema::class => 'array',
+            BooleanSchema::class => 'boolean',
+            NumberSchema::class => 'number',
+            ObjectSchema::class => 'object',
+            default => 'string',
+        };
     }
 }
